56f2bc4b5268971fdffcdd4175d13894e6211efe
Joshua Dominguez <joshuadominguez20@gmail.com>
2022-06-24 07:17:17 -0600

    created index.js and portfolioData.js

---
+++ src/portfolioData.js
@@ -0,0 +1,81 @@
+let portfolioData = {
+    "name": "Joshua Dominguez",
+    "role": "Full-Stack Web Development",
+    "linkedinId":"Your LinkedIn Id",
+    "githubid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"linkedin.com/in/joshua-dominguez-0b7419196/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/jcd2k",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"http://instagram.com/joshuacdominguez",
+          "className":"fa fa-insta"
+        }
+      ],
+    "about":"I am a 21 year old student from El Paso, TX. Currently I am studying to learn full stack web development through UTA Coding Bootcamp. My educational pursuits are in both finance and aerospace engineering. During my spare time, I enjoy going outdoors, playing my synthesizers, travelling or learning new things. My goal is to grow and expand upon my skills so that I may have the means to create positive and meaningful change.",
+    "work":[
+      {
+        "CompanyName":"Great American Steakburger",
+        "specialization":"Host/Server",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2021",
+      },
+      {
+        "CompanyName":"ENCON International, Inc.",
+        "specialization":"Administrative Assistant",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2020",
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"SQL"
+      }
+    ],
+    "projects":[
+      {
+        "name":"GreenPlanet Landscaping",
+        "description":"first project app using basic JS",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Psych Clinic Backend",
+        "description":"mockup backend for a psych clinic using SQL",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"EXPRESSN",
+        "description":"interface for artists and art admirers using MongoDB and ReactJS",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+}
+  
+export default portfolioData
